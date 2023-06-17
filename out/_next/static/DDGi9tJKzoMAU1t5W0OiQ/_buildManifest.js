d0258b6fa1fdb8ece3fb308c308f94db703a621e
Ryan.K <ryankung@ieee.org>
2023-06-17 17:31:52 +0800

    added output for gitpage

---
+++ out/_next/static/DDGi9tJKzoMAU1t5W0OiQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7b3c57c652a0f3a8.js"],"/_error":["static/chunks/pages/_error-3f6d1c55bb8051ab.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
