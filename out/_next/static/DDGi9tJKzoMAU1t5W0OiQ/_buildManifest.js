13d01490b5d60fb94f36bcab1d818e6827984512
Ryan.K <ryankung@ieee.org>
2023-11-30 00:08:06 +0800

    update export

--- out/_next/static/DDGi9tJKzoMAU1t5W0OiQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7b3c57c652a0f3a8.js"],"/_error":["static/chunks/pages/_error-3f6d1c55bb8051ab.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
