048b6a8648f06b4e8476a92bc28edf4a05c4f8cb
Ryan.K <ryankung@ieee.org>
2023-12-11 18:46:28 +0800

    update config

---
+++ out/_next/static/L9_owzFXaXmxCjLa186Bi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ad497d3fc7adf54d.js"],"/_error":["static/chunks/pages/_error-3f6d1c55bb8051ab.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
